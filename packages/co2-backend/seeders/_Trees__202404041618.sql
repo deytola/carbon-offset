935a06c98bcccc27fed759dc31308989eed7e29f
deytola <adebolaadetola65@gmail.com>
2024-04-04 16:20:47 -0600

    WIP: export orders, trees, and users data

---
+++ packages/co2-backend/seeders/_Trees__202404041618.sql
@@ -0,0 +1,2 @@
+INSERT INTO public."Trees" (species,age,"unitPrice","createdAt","updatedAt") VALUES
+	 ('oak',30,40000,'2024-04-01 13:27:11.083-06','2024-04-01 13:27:11.083-06');
---
+++ packages/co2-backend/seeders/_Users__202404041617.sql
@@ -0,0 +1,20 @@
+INSERT INTO public."Users" ("firstName","lastName",email,"password","userRole","createdAt","updatedAt") VALUES
+	 ('Ade','Tola','a@b.g','$2b$12$jTXSNa4avHzWAmFSIg9YYuuE6DwQHlvuoSeFsXn6M2/9nyI4cCF/2','regular','2024-04-01 13:20:54.851-06','2024-04-01 13:20:54.851-06'),
+	 ('Ade','Bola','a@b.c','$2b$12$ntQavzA80bqHZSRjWyCoG.KLChxHKlmB2hIWeh5wLbztVKDWGcHr.','regular','2024-04-02 08:11:17.928-06','2024-04-02 08:11:17.928-06'),
+	 ('Tola','Jide','b@c.d','$2b$12$MR3vgh5MBWxoNhWvkrkFfu8vkYCVpUJF28zUKqZgKUqydgYxM6o6W','regular','2024-04-02 08:42:39.777-06','2024-04-02 08:42:39.777-06'),
+	 ('Chris','Helmsworth','aaa@b.g','$2b$12$c2iR./oX6ki8JzBjmOJiGe4/Yix84L3fK2PA5iiOAW8yvWC/DI.nG','regular','2024-04-02 16:17:22.705-06','2024-04-02 16:17:22.705-06'),
+	 ('Chris','Helmsworth','aaaa@b.g','$2b$12$dfNrjsIbyh6mNEORPJjHweRLqYWyh81ai5es2ayCtnurs.wPpYxoW','regular','2024-04-02 16:20:57.853-06','2024-04-02 16:20:57.853-06'),
+	 ('Chris','Helmsworth','r@b.g','$2b$12$XUPW99cJRBz912RFeZqkiee.PK0ty6pu43lTA1xUiPl859uBovLXe','regular','2024-04-02 16:23:44.281-06','2024-04-02 16:23:44.281-06'),
+	 ('Chris','Helmsworth','rr@b.g','$2b$12$siVazBASJJ0M9tywxwn97.bZJQYYbOAG.bYw6azSLWWMEqoYRVzva','regular','2024-04-02 16:25:54.353-06','2024-04-02 16:25:54.353-06'),
+	 ('Chris','Helmsworth','user@nextmail.com','$2b$12$1N0RGpTpNvx8wAr8W9h0R.Mb6SKT9WniAmu/gJW1TDzqQKtVxJvNu','regular','2024-04-02 16:27:06.311-06','2024-04-02 16:27:06.311-06'),
+	 ('Chris','Helmsworth','rrrrr@b.g','$2b$12$Cx.9PLIWvUC9vms5bllPueH3I4a3eUnORGANS0x1az9JLTuI.3PQC','regular','2024-04-02 16:29:32.455-06','2024-04-02 16:29:32.455-06'),
+	 ('Chris','Helmsworth','t@b.g','$2b$12$VsNq728GLYxvrYO25LQEs.gpZzfeT7DSC.POUWaVv3GHPw9b8cnSu','regular','2024-04-02 16:30:34.065-06','2024-04-02 16:30:34.065-06');
+INSERT INTO public."Users" ("firstName","lastName",email,"password","userRole","createdAt","updatedAt") VALUES
+	 ('Chris','Helmsworth','f@b.g','$2b$12$kmbBSqlXDkjRxF1gCSAlauwGBKaS4dyhDUg/Vxu6DozlI2WSjegPO','regular','2024-04-02 16:33:57.793-06','2024-04-02 16:33:57.793-06'),
+	 ('Chris','Helmsworth','g@b.g','$2b$12$CeDte5OE8hxsaPlyWqQwIOh5RfJ.nwcgh3eL5MVoWdSrMr2A1etjS','regular','2024-04-02 16:37:25.213-06','2024-04-02 16:37:25.213-06'),
+	 ('Ade','Bola','g@b.c','$2b$12$Zzkfa5SZsa4/SjhDuevCUuk2RpGirX3TdF2UI.hLR8z.SjQckUlDq','regular','2024-04-03 10:26:52.04-06','2024-04-03 10:26:52.04-06'),
+	 ('Ade','Bola','h@b.c','$2b$12$QiSdrFlVTxuVXpaYuFIZXut1RK8dF3tTmNjR5jpHLph6zfFCXP2Xy','regular','2024-04-03 10:49:00.204-06','2024-04-03 10:49:00.204-06'),
+	 ('Ade','Bola','i@b.c','$2b$12$29Xec4WUnWWlRSwz0BeP3uT6WrSJTvLYrcCV55D0/8Z4nUve18aq6','regular','2024-04-03 10:50:01.885-06','2024-04-03 10:50:01.885-06'),
+	 ('Ade','Bola','j@b.c','$2b$12$g5TUJwVZvD72Exsa2a8L2.DDnuvLrvmGuShNiBTA7MF.GTvKA//F6','regular','2024-04-03 10:50:29.645-06','2024-04-03 10:50:29.645-06'),
+	 ('Ade','Bola','k@b.c','$2b$12$i8LtFfd00xu1P4Cd11RSYu4bsso.bYIMZitCq1uCAJz2rlZytf56O','regular','2024-04-03 10:51:37.461-06','2024-04-03 10:51:37.461-06'),
+	 ('Ade','Bola','l@b.c','$2b$12$/EgA0t3vfIYEODq2fWQtBuj0P27EawfaERd.75.a1agyMR5sn1Nna','regular','2024-04-03 10:55:29.26-06','2024-04-03 10:55:29.26-06');
